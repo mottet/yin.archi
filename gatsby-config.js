bacdd547cb73961f97b6b1817be9f3a47c4681ae
Marvin Mottet <11558282+mottet@users.noreply.github.com>
2022-01-09 15:05:02 +0100

    Kickoff of the portfolio with the Gatsby framework

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: `Yin Pei-Hua (Jocelyn) - Portfolio`,
+    description: `This is the portfolio of the interior designer Yin Pei-Hua. You will find here all the project she worked on!`,
+    author: `Mottet Marvin`,
+    siteUrl: `https://yin.archi/`,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage: React.FC = () => (
+  <Layout>
+    <Seo title="Yin Pei-Hua (Jocelyn) - Portfolio" />
+    <h1 className="text-3xl font-bold underline">Hi people</h1>
+    <p>Welcome to the portfolio of Yin Pei-Hua.</p>
+  </Layout>
+)
+
+export default IndexPage
