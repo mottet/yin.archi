3a6d95322c24afd1b963962a1df9a0c19b68b956
Marvin Mottet <11558282+mottet@users.noreply.github.com>
2024-10-22 22:38:27 +0200

    Upgrade Next.js to v15

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+// import { useState } from 'react'
+// import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
+// import { Textarea } from "@/components/ui/textarea"
 import { Card, CardContent } from "@/components/ui/card"
 import { Instagram, Linkedin, Mail } from "lucide-react"
 
